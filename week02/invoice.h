8a1c0b79582c034b62c1e19eebece4dbcbbfba5a
201930425 <012010@email.daelim.ac.kr>
2025-09-04 11:25:12 +0900

    modified *.h *.cpp & company.h company.cpp

---
+++ week02/invoice.h
@@ -0,0 +1,23 @@
+﻿/**************************************************************
+ * Invoice 클래스의 인터페이스 파일                           *
+ **************************************************************/
+#ifndef INVOICE_H
+#define INVOICE_H
+#include "product.h"
+#include "company.h"
+
+class Invoice
+{
+  private:
+    int invoiceNumber;
+    double invoiceTotal; 
+    Company company;
+  public: 
+    Invoice(int invoiceNumber, string name, string phone);
+    //Invoice(int invoiceNumber, Company company);
+    ~Invoice(); 
+    void add(int quantity, Product product);
+    void print() const;
+    //void print(Company company) const;
+};
+#endif
