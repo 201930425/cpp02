980cac9db79f04f226d59a4bffb9ef4c6739f4e2
201930425 <012010@email.daelim.ac.kr>
2025-09-25 11:32:41 +0900

    week05 v0.8 unsafe casting

--- week05/app01.cpp
+++ week05/app01.cpp
@@ -20,12 +20,20 @@ int main()
 {
 	Animal* pa =  new Dog(); //upcasting
 	pa->makeSound();
-	//Dog* pd = (Dog*)pa; //downcasting, oldstyle (C style)
-	Dog* pd = dynamic_cast<Dog*>(pa); //downcasting, modern style (C++11)
-	pd->makeSound();
 
-	delete pd; 
-	pd = nullptr;
+	Cat* pc = (Cat*)pa; //Dangerous begavior
+	pc->makeSound();
+	cout << pc << endl;
+	delete pc;
+	pc = nullptr;
+
+	////Dog* pd = (Dog*)pa; //downcasting, oldstyle (C style)
+	//Dog* pd = dynamic_cast<Dog*>(pa); //downcasting, modern style (C++11)
+	//pd->makeSound();
+
+	
 
 	return 0;
 }
+
+//git reset --hardHead~삭제할커밋개수
