a3db27c4e49013d96042ef84cccb6f584dc1bd8c
201930425 <012010@email.daelim.ac.kr>
2025-11-20 12:20:09 +0900

    week13 v3.7 function template

--- week13/app01.cpp
+++ week13/app01.cpp
@@ -2,25 +2,21 @@
 using namespace std;
 
 template <typename T>
-void exchange(T* first, T* second)
+T bigger(T first, T second)
 {
-	T temp = *first;
-	*first = *second;
-	*second = temp;
+	if (first > second) {
+		return first;
+	}
+	return second;
 }
-
 int main()
 {
 	int i1 = 11;
-	int i2 = 76;
-	exchange(&i1, &i2);
-	cout << "swap(11, 76) = ";
-	cout << i1 << "  " << i2 << endl;
-
-	double d1 = 51.5;
-	double d2 = 42.7;
-	exchange(&d1, &d2);
-	cout << "swap(51.5, 42.7) = ";
-	cout << d1 << "  " << d2 << endl;
+	double d1 = 11.97;
+	//cout << bigger(i1, d1) << endl;
+	cout << bigger<double>(i1, d1) << endl; //명시적 호출
+	cout << bigger<double>(55.7, 11.9) << endl;
+	cout << bigger(55.7, 11.9) << endl;
+	cout << bigger<string>("Hxllo", "Hi") << endl;
 	return 0;
 }
